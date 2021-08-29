548377804356f76d2017355f8ca2817a9029ec1e
Tim H <timh1203@gmail.com>
2021-08-28 22:51:36 -0500

    Add Banner section

--- src/Banner.js
+++ src/Banner.js
@@ -1,12 +1,47 @@
 import styled from 'styled-components';
 import Wrapper from './Wrapper';
+import Image from 'next/image';
 
-const H1 = styled.h1``;
+const Hero = styled.section`
+  position: relative;
+  text-align: center;
+  color: white;
+  font-family: var(--nunitoB);
+`;
+const Image1 = styled(Image)`
+  object-fit: contain;
+  object-fit: cover;
+  opacity: 0.8;
+`;
+const Title = styled.p`
+  position: absolute;
+  top: 25%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 9.6rem;
+`;
+const Subtitle = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 3.2rem;
+`;
 
 export default function Banner() {
   return (
     <Wrapper>
-      <H1>Banner Section</H1>
+      <Hero>
+        <Image1
+          src='/banner.jpg'
+          alt='Icarus Initiative Hero'
+          width={3360}
+          height={1080}
+          quality={100}
+        />
+        <Title>Icarus Initiative</Title>
+        <Subtitle>The Crowdsourced Solana/Rust Learning Initiative</Subtitle>
+      </Hero>
     </Wrapper>
   );
 }
