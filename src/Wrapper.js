e9919a032a20bec30819fb953b567d09cc9321a5
Tim H <timh1203@gmail.com>
2021-08-28 20:44:16 -0500

    Add styled components and Wrapper

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,3 +1,38 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    --grey: #26262b;
+    --gray: var(--grey);
+    --white: #ffffff;
+    --pageFontColor: var(--white);
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    background: var(--grey);
+    color: var(--pageFontColor);
+    padding: 0;
+    margin: 0;
+    font-size: 1.6rem;
+    line-height:2;
+  }
+  h1 { font-size: 2.8rem; }
+  h2 { font-size: 2.4rem; }
+  h3 { font-size: 2rem; }
+`;
+const Wrapper = styled.main`
+  text-align: center;
+`;
+
 export default function Page({ data, children }) {
-  return <main style={{ color: 'red' }}>{children}</main>;
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      {children}
+    </Wrapper>
+  );
 }
