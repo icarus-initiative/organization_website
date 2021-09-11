7bbb3b13f865cadbb2f8b5dfb84e1de238ed2630
Tim H <timh1203@gmail.com>
2021-09-10 19:46:28 -0500

    Update Process

--- src/Process.js
+++ src/Process.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Wrapper from './Wrapper';
+import globals from './data';
 
 const Section = styled.section`
   margin: 0 auto;
@@ -53,6 +54,7 @@ const Purplerize = styled.span`
   color: var(--purple);
 `;
 export default function Process() {
+  const { process } = globals();
   return (
     <Wrapper>
       <Section id='process'>
@@ -61,13 +63,17 @@ export default function Process() {
         </H1>
         <Ul>
           <Li>
-            Icarus seeks pilots who are <Colorize>passionate</Colorize>,{' '}
+            Icarus seeks pilots who are <Colorize>leaders</Colorize>,{' '}
             <Colorize>builders</Colorize> and <Colorize>contributors</Colorize>{' '}
-            back to the community.
+            to enrich the community.
           </Li>
           <Li>
             If you're expecting a bootcamp and a golden ticket, this is{' '}
             <em>not</em> it.
+          </Li>
+          <Li>
+            Icarus seeks people who are passionate about mastering their craft
+            because the future is built by meticulous minds.
           </Li>
         </Ul>
         <Cards>
@@ -75,19 +81,20 @@ export default function Process() {
             <CardTitle>1. Join the Icarus Initiative Discord</CardTitle>
             <CardDescription>
               <li>
-                Get connected into the community as a member to interact with
-                members.
+                Get connected into the community as a member to interact and
+                learn what it's about.
               </li>
               <li>
                 Visit{' '}
                 <a
-                  href='https://discord.gg/9yYsQVYR3K'
+                  href={process.discord.url}
+                  title={process.discord.title}
                   rel='noopener noreferrer nofollow'
                   target='_blank'
                 >
                   Icarus Discord
                 </a>{' '}
-                and send an @ to an admin at the entrance.
+                and send an @ to an admin at the entrance. Be patient. 😃
               </li>
             </CardDescription>
           </Card>
@@ -95,8 +102,16 @@ export default function Process() {
             <CardTitle>2. Complete the Figment Solana Protocol</CardTitle>
             <CardDescription>
               <li>
-                Finish the figment course to learn the basics before you start
-                expeditions.
+                Finish the{' '}
+                <a
+                  href={process.figment.url}
+                  title={process.figment.title}
+                  rel='noopener noreferrer nofollow'
+                  target='_blank'
+                >
+                  figment course
+                </a>{' '}
+                to learn the basics while you wait to start expeditions.
               </li>
               <li>
                 This also sets you up with a DataHub account and get familiar
@@ -105,7 +120,7 @@ export default function Process() {
             </CardDescription>
           </Card>
           <Card>
-            <CardTitle>3. Find Or Make A Squad</CardTitle>
+            <CardTitle>3. Find or Make A Squad</CardTitle>
             <CardDescription>
               <li>
                 If enlisting as a captain, notify an admin to help you create a
@@ -114,7 +129,8 @@ export default function Process() {
               <li>
                 If enlisting as a pilot, scroll through{' '}
                 <a
-                  href='https://github.com/orgs/icarus-initiative/teams'
+                  href={process.teams.url}
+                  title={process.teams.title}
                   rel='noopener noreferrer nofollow'
                   target='_blank'
                 >
@@ -124,13 +140,13 @@ export default function Process() {
                 (under team description).
               </li>
               <li>
-                Introduce yourself and the captain (aka Maintainer) will handle
-                the recruitment.
+                Introduce yourself and the captain(s) (aka Maintainer) will
+                handle the recruitment.
               </li>
             </CardDescription>
           </Card>
           <Card>
-            <CardTitle>4. Meet Squad And Chart Path</CardTitle>
+            <CardTitle>4. Meet Squad and Chart Path</CardTitle>
             <CardDescription>
               <li>
                 Captains will schedule a squad briefing in the discord to chart
@@ -141,48 +157,74 @@ export default function Process() {
                 pitstops, resources to evaluate and other details.
               </li>
               <li>
-                The captains also collect pilot information for the roster,
-                submit a PR for all the dates, pilots, and other details for the
-                green light.
+                The captains also collect pilot information for the{' '}
+                <a
+                  href={process.squadrons.url}
+                  title={process.squadrons.title}
+                  rel='noopener noreferrer nofollow'
+                  target='_blank'
+                >
+                  roster
+                </a>{' '}
+                and will submit a PR for all the dates, pilots, and other key
+                details.
               </li>
+              <li>Await for the green light to takeoff!</li>
             </CardDescription>
           </Card>
           <Card>
-            <CardTitle>5. Start Expedition And Take Pitstops</CardTitle>
+            <CardTitle>5. Start Expedition and Take Pitstops</CardTitle>
             <CardDescription>
               <li>
                 Expeditions have pitstops, where pilots reconvene on discord to
                 discuss project and resource details.
               </li>
               <li>
-                Pilots also encouraged to twitter hashtag their squadmates and
-                icarusinitiative (once available).
+                Pilots also encouraged to tweet at their squadmates and at{' '}
+                <a
+                  href={process.twitter.url}
+                  title={process.twitter.title}
+                  rel='noopener noreferrer nofollow'
+                  target='_blank'
+                >
+                  #icarusinitiative
+                </a>
+                .
               </li>
             </CardDescription>
           </Card>
           <Card>
-            <CardTitle>6. Landing And Debriefing</CardTitle>
+            <CardTitle>6. Landing and Debriefing</CardTitle>
             <CardDescription>
               <li>
                 Squadrons lands at the end of the expedition and presents their
-                flight.
+                journey.
               </li>
               <li>Captains to set up a meeting with a more senior squadron.</li>
               <li>
                 Senior squadrons gives feedback and notify admins via a PR to
-                report the "Actual Landing Date".
+                report the{' '}
+                <a
+                  href={process.squadrons.url}
+                  title={process.squadrons.title}
+                  rel='noopener noreferrer nofollow'
+                  target='_blank'
+                >
+                  Actual Landing Date
+                </a>
+                .
               </li>
             </CardDescription>
           </Card>
           <Card>
-            <CardTitle>7. Vote, Rest, Redeploy</CardTitle>
+            <CardTitle>7. Vote, Rest, and Redeploy</CardTitle>
             <CardDescription>
               <li>
                 Captains collects majority votes to vote resources into the
-                expdition's open positions.
+                expdition's open slots.
               </li>
               <li>
-                Squadrons, captains, or pilots can create content for
+                Squadrons, captains, or pilots can opt to create content for
                 contribution.
               </li>
               <li>
