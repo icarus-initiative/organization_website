de4a60161966b66f8d858024c9b521ee34bfee7f
Tim H <timh1203@gmail.com>
2021-09-10 19:46:18 -0500

    Update Credits

--- src/Credits.js
+++ src/Credits.js
@@ -1,37 +1,90 @@
 import React from 'react';
+import globals from './data';
 
 export default function Credits() {
+  const { credits } = globals();
   return (
     <section>
-      Icon Credits:{' '}
+      Icon credits to{' '}
       <a
-        href='https://www.flaticon.com/authors/pixel-perfect'
-        title='Pixel perfect'
+        href={credits.flaticon.url}
+        title={credits.flaticon.title}
+        rel='noopener noreferrer nofollow'
+        target='_blank'
       >
-        Pixel perfect
+        {credits.flaticon.title}
       </a>
-      ,{' '}
-      <a href='https://www.flaticon.com/authors/smashicons' title='Smashicons'>
-        Smashicons
-      </a>
-      ,{' '}
-      <a href='https://www.freepik.com' title='Freepik'>
-        Freepik
+      :{' '}
+      <a
+        href={credits.nhorphai.url}
+        title={credits.nhorphai.title}
+        rel='noopener noreferrer nofollow'
+        target='_blank'
+      >
+        {credits.nhorphai.title}
       </a>
       ,{' '}
       <a
-        href='https://www.flaticon.com/authors/dimitry-miroliubov'
-        title='Dimitry Miroliubov'
+        href={credits.freepik.url}
+        title={credits.freepik.title}
+        rel='noopener noreferrer nofollow'
+        target='_blank'
       >
-        Dimitry Miroliubov
+        {credits.freepik.title}
       </a>
       ,{' '}
-      <a href='https://www.flaticon.com/authors/icongeek26' title='Icongeek26'>
-        Icongeek26
+      <a
+        href={credits.eucalyp.url}
+        title={credits.eucalyp.title}
+        rel='noopener noreferrer nofollow'
+        target='_blank'
+      >
+        {credits.eucalyp.title}
       </a>
       ,{' '}
-      <a href='https://www.freepik.com' title='Freepik'>
-        Freepik
+      <a
+        href={credits.pixelperfect.url}
+        title={credits.pixelperfect.title}
+        rel='noopener noreferrer nofollow'
+        target='_blank'
+      >
+        {credits.pixelperfect.title}
+      </a>
+      ,{' '}
+      <a
+        href={credits.smashicons.url}
+        title={credits.smashicons.title}
+        rel='noopener noreferrer nofollow'
+        target='_blank'
+      >
+        {credits.smashicons.title}
+      </a>
+      ,{' '}
+      <a
+        href={credits.dimitry.url}
+        title={credits.dimitry.title}
+        rel='noopener noreferrer nofollow'
+        target='_blank'
+      >
+        {credits.dimitry.title}
+      </a>
+      ,{' '}
+      <a
+        href={credits.goodware.url}
+        title={credits.goodware.title}
+        rel='noopener noreferrer nofollow'
+        target='_blank'
+      >
+        {credits.goodware.title}
+      </a>
+      ,{' '}
+      <a
+        href={credits.icongeek26.url}
+        title={credits.icongeek26.title}
+        rel='noopener noreferrer nofollow'
+        target='_blank'
+      >
+        {credits.icongeek26.title}
       </a>
     </section>
   );
